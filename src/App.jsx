25944c0c674ed23a3a5afc160c5f1864a1ecf8eb
abhaypatidar3 <patidarabhay1996@gmail.com>
2025-08-07 00:41:54 +0530

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header'
-import Slider from './components/Slider'
-import ProductionHouse from './components/ProductionHouse'
-import GenreMovieList from './components/GenreMovieList'
+import Header from './components/header.jsx'
+import Slider from './components/Slider.jsx'
+import ProductionHouse from './components/ProductionHouse.jsx'
+import GenreMovieList from './components/GenreMovieList.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
